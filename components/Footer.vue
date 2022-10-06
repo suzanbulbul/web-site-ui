78045a89a75f247779f649e40884590f36488fc7
Suzan <suzanbulbl@gmail.com>
2022-10-06 16:06:52 +0300

    footer strağ

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,51 +3,59 @@
     <div class="container">
      <div class="row">
         <div class="col-md-4 col-sm-12">
-            <img class="logo" src="~/assets/img/logo.png" />
-            <div class="d-block d-sm-block d-md-none">
-              <div class="subtext">İsmet Paşa Mahallesi Trabzon Caddesi No:31/B Dulkadiroğlu /K Maraş</div>
+            <div v-if="data.logo">
+              <img class="logo" :src="`${$config.strapiURL}${data.logo.data.attributes.url}`" alt="Banner">
             </div>
             <div class="social-media">
                 <div class="row justify-content-start align-items-center">
-                <a v-for="(social, idx) in socialmedia" :key="idx" :href="social.url" target="_blank">
+                <a v-for="(social, idx) in data.socialmedia" :key="idx" :href="social.url" target="_blank">
                   <i :class="`icon-${social.icon}`" />
                 </a>
+            </div>
+             <div class="d-block d-sm-block d-md-none">
+              <div class="subtext">İsmet Paşa Mahallesi Trabzon Caddesi No:31/B Dulkadiroğlu /K Maraş</div>
             </div>
         </div>
         </div>
         <div class="col-md-8">
           <ul class="row justify-content-end align-items-start">
-            <li v-for="(item, idx) in 4" :key="idx" class="list">
-              <div class="title">
-                test
+            <li v-for="(item, idx) in menu" :key="idx" class="list">
+              <div v-if="item.items.length > 0" class="title">
+                {{item.title}}
+                <ul class="mt-2">
+                  <li v-for="(str, index) in item.items" :key="index">
+                    <nuxt-link class="subtitle" :to="localePath(str.path)">{{ str.title }}</nuxt-link>
+                  </li>
+                </ul>
               </div>
-              <ul >
-                <li v-for="(str, index) in 2" :key="index">
-                  <a class="subtitle" >
-                    deneme
-                  </a>
-                </li>
-              </ul>
+              <nuxt-link class="title" v-else :to="localePath(item.path)">{{ item.title }}</nuxt-link>
             </li>
           </ul>
         </div>
      </div>
      <div class="d-none d-md-block d-sm-none">
-      <div class="subtext">İsmet Paşa Mahallesi Trabzon Caddesi No:31/B Dulkadiroğlu /K Maraş</div>
+      <div class="subtext" v-html="data.subtext"></div>
      </div>
     </div>
   </footer>
 </template>
 <script>
   export default {
-    data () {
-      return {
-        socialmedia: [
-          { icon: 'instagram', url: '/' },
-          { icon: 'whatsapp', url: '/' },
-          { icon: 'phone', url: '/' }
-        ]
+      data () {
+          return {
+              data: '',
+              menu: ''
+          }
+      },
+      mounted () {
+        this.$axios.$get(`navigation/render/4?locale=${this.$i18n.locale}&type=TREE`).then((res) => {
+                this.menu = res
+        }),
+        this.$axios.$get(`footer?populate=logo,socialmedia&locale=${this.$i18n.locale}`).then((res) => {
+            if (res.data && res.data.attributes) {
+                this.data = res.data.attributes
+            }
+        })
       }
-    }
   }
   </script>
