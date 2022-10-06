068df711e5e0e6119234d6aa37133b70e3f33244
Suzan <suzanbulbl@gmail.com>
2022-10-06 09:47:23 +0300

    strapi add for anout-us

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -1,14 +1,29 @@
 <template>
     <div class="about-us mb-100">
         <div class="row align-items-center">
-            <div class="col-md-6 col-sm-12 text-center">
-                <img src="~/assets/img/example.png" />
+            <div class="col-md-6 col-sm-12 text-center" v-if="data.img.data">
+                <img :src="`${$config.strapiURL}${data.img.data.attributes.url}`" alt="Banner">
             </div>
             <div class="col-md-6 col-sm-12">
-                <div class="sub-title">Hakkımızda</div>
-                <p class="desc bold">Omega 1992 den beri, banyo aksesuarları sektöründeki seçkin dünya markalarını ithalat ederek, distribütörlük hizmeti veren bir satış ve pazarlama şirketidir..</p>
-                <p class="desc">Konut banyo aksesuarları ana grubunda Ispanyol Sonia ve Windisch, Italyan Gedy markalarının yanısıra, Banomega konsepti altında 8 ayrı yeni markayı 2012 senesinde ürün yelpazesi katmıştır. Genel Kullanım Banyo ve WC aksesuarları grubunda Amerikan Bobrick markasına ek olarak 2014 senesinde Dyson, Vola ve Ophardt marka ürünleri satış portföyüne eklenmiştir.</p>
+                <div class="sub-title">{{Hakkımızda}}</div>
+                <p class="desc" v-html="data.desc"></p>
             </div>
         </div>
     </div>
 </template>
+<script>
+    export default {
+      data () {
+        return {
+          data: ''
+        }
+      },
+      mounted () {
+        this.$axios.$get(`about-us?populate=aboutus,aboutus.img&locale`).then((res) => {
+          if (res.data && res.data.attributes) {
+            this.data = res.data.attributes.aboutus
+          }
+        })
+      }
+    }
+    </script>
--- components/Banner.vue
+++ components/Banner.vue
@@ -1,39 +1,39 @@
 <template>
-   <div class="banner mb-100">
-    <carousel 
-        :per-page="1"  
-        :mouse-drag="true" 
-        :pagination-enabled="false" 
-        :navigation-enabled="true"
-        navigationNextLabel=""
-        navigationPrevLabel=""
-   >
-        <slide  v-for="(item, index) in data" :key="index" >
-            <div v-if="item.img.data">
-                <img :src="`${$config.strapiURL}${item.img.data.attributes.url}`" alt="Banner">
-            </div>
-            <div v-if="item.title && item.desc" class="area">
-                <h1 class="title">{{item.title}}</h1>
-                <p class="desc w-80" v-html="item.desc"></p>
-            </div>
-        </slide>
-    </carousel>
-   </div>
-  </template>
+  <div class="banner mb-100">
+  <carousel 
+      :per-page="1"  
+      :mouse-drag="true" 
+      :pagination-enabled="false" 
+      :navigation-enabled="true"
+      navigationNextLabel=""
+      navigationPrevLabel=""
+  >
+    <slide  v-for="(item, index) in data" :key="index" >
+        <div v-if="item.img.data">
+            <img :src="`${$config.strapiURL}${item.img.data.attributes.url}`" alt="Banner">
+        </div>
+        <div v-if="item.title && item.desc" class="area">
+            <h1 class="title">{{item.title}}</h1>
+            <p class="desc w-80" v-html="item.desc"></p>
+        </div>
+    </slide>
+  </carousel>
+  </div>
+</template>
 <script>
-    export default {
-      data () {
-        return {
-          data: ''
-        }
-      },
-      mounted () {
-        this.$axios.$get(`banner?populate=slider,slider.img&locale`).then((res) => {
-          if (res.data && res.data.attributes) {
-            this.data = res.data.attributes.slider
-          }
-        })
-      }
+export default {
+  data () {
+    return {
+      data: ''
     }
-    </script>
+  },
+  mounted () {
+    this.$axios.$get(`banner?populate=slider,slider.img&locale`).then((res) => {
+      if (res.data && res.data.attributes) {
+        this.data = res.data.attributes.slider
+      }
+    })
+  }
+}
+</script>
   
